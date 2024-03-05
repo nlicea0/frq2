f63b900ff016761cdc52ea9c28d75e94717b4fe0
Nikki Licea <nlicea@usc.edu>
2024-03-04 19:29:29 -0800

    Add code for race.c

---
+++ race.c
@@ -0,0 +1,52 @@
+#include "types.h"
+#include "stat.h"
+#include "user.h"
+#include "condvar.h"
+#include "fcntl.h"
+
+int main(){
+    // struct spinlock lk;
+    // init_lock(&lk);
+    // lock(&lk);
+
+    struct condvar cv;
+    int fd = open("flag", O_RDWR | O_CREATE);
+    init_lock(&cv.lk);
+
+    // critical section
+    int pid = fork();
+    if (pid < 0){
+        printf(1, "Error forking first child.\n");
+    }
+    else if (pid == 0){
+        sleep(5);
+        printf(1, "Child 1 Executing\n");
+        lock(&cv.lk);
+        write(fd, "done", 4);
+        cv_signal(&cv);
+        unlock(&cv.lk);
+    }
+    else {
+        pid = fork();  // fork the first child
+        if (pid < 0){
+            printf(1, "Error forking second child.\n");
+        }
+        else if (pid == 0){
+            lock(&cv.lk);
+            cv_signal(&cv);
+            unlock(&cv.lk);
+            printf(1, "Child 2 Executing\n");
+        }
+        else {
+            printf(1, "Parent Waiting\n");
+            int i;
+            for(i = 0; i < 2; i++){
+                wait();
+            }
+            printf(1, "Children completed\n");
+            printf(1,"Parent Executing\n");
+            printf(1,"Parent exiting.\n");
+        }
+    }
+    exit();
+}
